dcd4839d76c3292fd621891d2401b159ac15acb1
lislis <mail@lislis.de>
2026-08-07 12:57:27 +0200

    config token lifetimes

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,22 @@
-module.exports = () => ({});
+module.exports = () => ({
+  'users-permissions': {
+    config: {
+      jwtManagement: 'refresh',
+      sessions: {
+        accessTokenLifespan: 3000, // 50 minutes
+        maxRefreshTokenLifespan: 2592000, // 30 days (default)
+        idleRefreshTokenLifespan: 1209600, // 14 days (default)
+        maxSessionLifespan: 432000, // 5 days 
+        idleSessionLifespan:  36000, // 10 hours
+        httpOnly: false, // Set to true for HTTP-only cookies
+        cookie: {
+          name: 'strapi_up_refresh',
+          sameSite: 'strict',
+          path: '/',
+          secure: false, // true in production
+        },
+      },
+    },
+  },
+
+});
